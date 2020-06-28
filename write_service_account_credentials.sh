00401a5f2a5d1be9c9a4e43641ded02a4c9a06b9
Nic Cheneweth <nchenewe@thoughtworks.com>
2020-06-28 15:33:12 -0500

    [ncheneweth] secret write test iteration

--- write_service_account_credentials.sh
+++ write_service_account_credentials.sh
@@ -4,5 +4,5 @@
 if !(secrethub tree twdps/di/svc/aws/dps-1 | grep -q "DPSSimpleServiceAccount"); then
     secrethub mkdir twdps/di/svc/aws/dps-1/DPSSimpleServiceAccount
 fi
-secrethub run -- terraform output DPSSimpleServiceAccount_encrypted_aws_secret_access_key | base64 --decode | gpg -dq | secrethub write twdps/di/svc/aws/dps-1/DPSSimpleServiceAccount/aws-secret-access-key
+secrethub run -- terraform output DPSSimpleServiceAccount_encrypted_aws_secret_access_key | base64 -d | gpg -dq | secrethub write twdps/di/svc/aws/dps-1/DPSSimpleServiceAccount/aws-secret-access-key
 secrethub run -- terraform output DPSSimpleServiceAccount_aws_access_key_id | base64 --decode | gpg -dq | secrethub write twdps/di/svc/aws/dps-1/DPSSimpleServiceAccount/aws-access-key-id
