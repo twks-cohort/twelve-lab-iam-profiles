b9289fcec8a986f71ac77fb71346e717c512b200
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-05-17 17:16:15 -0500

    [ncheneweth] test new count-based profile deployment

---
+++ scripts/import_gpg_keys.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+secrethub read --out-file .public.key twdps/di/svc/gpg/public.key
+secrethub read --out-file .private.key twdps/di/svc/gpg/private.key
+
+gpg --import .public.key
+gpg --passphrase ${GPG_KEY_PASSPHRASE} --import .private.key
