67cd13fdd8f74ce21ba4a1203f077837cebf72b4
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-09-05 12:49:40 -0500

    [ncheneweth] new orb version

--- scripts/write_service_account_credentials.sh
+++ scripts/write_service_account_credentials.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+terraform init
 secrethub run -- terraform output DPSSimpleServiceAccount_encrypted_aws_secret_access_key | base64 -d | gpg -dq --passphrase ${GPG_KEY_PASSPHRASE} | secrethub write twdps/di/svc/aws/dps-2/DPSSimpleServiceAccount/aws-secret-access-key
 secrethub run -- terraform output DPSSimpleServiceAccount_aws_access_key_id | sed 's/"//g' | secrethub write twdps/di/svc/aws/dps-2/DPSSimpleServiceAccount/aws-access-key-id
