7a30a8a790f150a3e211ac8b001978ac2f63e431
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-09-05 12:53:55 -0500

    [ncheneweth] new orb version

--- scripts/import_gpg_keys.sh
+++ scripts/import_gpg_keys.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo ${GPG_KEY_PASSPHRASE:0:5}
 
 secrethub read --out-file .public.key twdps/di/svc/gpg/public.key
 secrethub read --out-file .private.key twdps/di/svc/gpg/private.key
