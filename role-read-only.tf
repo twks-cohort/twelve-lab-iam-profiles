eeb1fe071b6526fe75b140f3ce6962e032f017fe
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-01-21 16:28:01 -0600

    [ncheneweth] pipeline job rename

--- role-read-only.tf
+++ role-read-only.tf
@@ -1,11 +1,12 @@
 module "DPSReadOnlyRole" {
   create_role = true
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "~> 4.10.1"
+  source      = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version     = "~> 4.10"
 
-  role_name = "DPSReadOnlyRole"
-  role_requires_mfa = false
-  custom_role_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+  role_name                         = "DPSReadOnlyRole"
+  role_requires_mfa                 = false
+  custom_role_policy_arns           = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+  number_of_custom_role_policy_arns = 1
 
   trusted_role_arns = [
     "arn:aws:iam::${var.nonprod_account_id}:root",
--- role-terraform.tf
+++ role-terraform.tf
@@ -1,11 +1,12 @@
 module "DPSTerraformRole" {
   create_role = true
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "~> 4.10.1"
+  source      = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version     = "~> 4.10.1"
 
-  role_name = "DPSTerraformRole"
-  role_requires_mfa = false
-  custom_role_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  role_name                         = "DPSTerraformRole"
+  role_requires_mfa                 = false
+  custom_role_policy_arns           = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  number_of_custom_role_policy_arns = 1
 
   trusted_role_arns = [
     "arn:aws:iam::${var.nonprod_account_id}:root",
