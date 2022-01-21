2f42e31ff4c790f214be99f183644375020b9ce4
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-01-21 16:19:52 -0600

    [ncheneweth] pipeline job rename

--- role-read-only.tf
+++ role-read-only.tf
@@ -1,7 +1,7 @@
 module "DPSReadOnlyRole" {
   create_role = true
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "~> 4.7"
+  version = "~> 4.10.1"
 
   role_name = "DPSReadOnlyRole"
   role_requires_mfa = false
