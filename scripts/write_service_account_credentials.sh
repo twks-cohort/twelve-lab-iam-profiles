c07e1f55239fdd3d1089735aa5c1549a6efb832d
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-01-06 16:20:50 -0600

    [nc] rotate service account keys
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/write_service_account_credentials.sh
+++ scripts/write_service_account_credentials.sh
@@ -9,5 +9,5 @@ terraform output DPSNonprodServiceAccount_encrypted_aws_secret_access_key | sed 
 terraform output DPSNonprodServiceAccount_aws_access_key_id | tr -d \\n | sed 's/"//g' | opw write aws-dps-2 DPSNonprodServiceAccount-aws-access-key-id -
 
 # write the prod service account credentials to secret store
-terraform output DPSProdServiceAccount_encrypted_aws_secret_access_key | base64 -d | gpg -dq --passphrase ${GPG_KEY_PASSPHRASE} |  opw write aws-dps-2 DPSProdServiceAccount-aws-secret-access-key -
+terraform output DPSProdServiceAccount_encrypted_aws_secret_access_key | sed 's/"//g' | base64 -d | gpg -dq --passphrase ${GPG_KEY_PASSPHRASE} |  opw write aws-dps-2 DPSProdServiceAccount-aws-secret-access-key -
 terraform output DPSProdServiceAccount_aws_access_key_id | tr -d \\n | sed 's/"//g' | opw write aws-dps-2 DPSProdServiceAccount-aws-access-key-id -
