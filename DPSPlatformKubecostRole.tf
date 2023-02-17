c25814816ffd10082b579a4d406d10aebf8f3ccd
Bryan Oliver <boliver@linux.com>
2023-02-17 16:58:16 -0500

    add iam:GetOpenIDConnectProvider to kubecost

--- DPSPlatformKubecostRole.tf
+++ DPSPlatformKubecostRole.tf
@@ -43,7 +43,8 @@ resource "aws_iam_policy" "DPSPlatformKubecostRolePolicy" {
           "s3:Get*",
           "s3:List*",
           "eks:DescribeCluster",
-          "iam:ListOpenIDConnectProviders"
+          "iam:ListOpenIDConnectProviders",
+          "iam:GetOpenIDConnectProvider
         ]
         "Effect": "Allow"
         "Resource": "*"
