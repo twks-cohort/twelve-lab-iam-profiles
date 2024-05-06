13387aa06397f2517a14c8b387fb4a309f38d0ff
yue.yang <yue.yang@thoughtworks.com>
2024-05-06 10:44:44 +0800

    refine write_service_account_credentials.sh

--- scripts/write_service_account_credentials.sh
+++ scripts/write_service_account_credentials.sh
@@ -5,5 +5,6 @@ terraform init
 # secrethub run -- terraform output DPSSimpleServiceAccount_aws_access_key_id | sed 's/"//g' | secrethub write twdps/di/svc/aws/dps-2/DPSSimpleServiceAccount/aws-access-key-id
 
 # write the non-prod service account credentials to secret store
-# op item create --category "API Credential" --title "twelve-aws" --vault cohorts DPSNonprodServiceAccount-aws-secret-access-key=$(terraform output DPSNonprodServiceAccount_encrypted_aws_secret_access_key | sed 's/"//g' | base64 -d | gpg -dq --passphrase ${GPG_KEY_PASSPHRASE}) DPSNonprodServiceAccount-aws-access-key-id=$(terraform output DPSNonprodServiceAccount_aws_access_key_id | tr -d \\n | sed 's/"//g')
-op item edit "twelve-aws" --vault cohorts DPSNonprodServiceAccount-aws-secret-access-key=$(terraform output DPSNonprodServiceAccount_encrypted_aws_secret_access_key | sed 's/"//g' | base64 -d | gpg -dq --passphrase ${GPG_KEY_PASSPHRASE}) DPSNonprodServiceAccount-aws-access-key-id=$(terraform output DPSNonprodServiceAccount_aws_access_key_id | tr -d \\n | sed 's/"//g')
+access_key=$(terraform output DPSNonprodServiceAccount_encrypted_aws_secret_access_key | sed 's/"//g' | base64 -d | gpg -dq --passphrase ${GPG_KEY_PASSPHRASE})
+access_key_id=$(terraform output DPSNonprodServiceAccount_aws_access_key_id | tr -d \\n | sed 's/"//g')
+op item edit "twelve-aws" --vault cohorts DPSNonprodServiceAccount-aws-secret-access-key=$access_key DPSNonprodServiceAccount-aws-access-key-id=$access_key_id
